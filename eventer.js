957051778b2c61997a47d3c25263aea88020ad04
sevic6 <zeljkosevic95@gmail.com>
2017-01-15 07:50:25 +0100

    added pictures for results

--- eventer.js
+++ eventer.js
@@ -1,6 +1,6 @@
 module.exports=function(eventID,name,token,done){
 	var request=require("request");
-	var access="https://graph.facebook.com/v2.8/"+eventID+"/attending?limit=500&access_token="+token;
+	var access="https://graph.facebook.com/v2.8/"+eventID+"/attending?fields=name,picture{url}&limit=500&access_token="+token;
 	var regex=new RegExp(" ?"+name+" ");
 	var arr=[];
 	var giveMeData=function(access){
@@ -9,8 +9,9 @@ module.exports=function(eventID,name,token,done){
 				var paging=JSON.parse(body).paging;
 				var res=JSON.parse(body).data;	
 				for(var k in res){
-					if(res[k]["name"].search(regex)>-1)
+					if(res[k]["name"].search(regex)>-1){
 						arr.push(res[k]);
+					}
 				}
 				if(paging.next===undefined){
 					return done(null,arr);
