db409aa01272dcdd3a8add1fdbd141686de96cda
zsevic <zeljkosevic95@gmail.com>
2017-02-07 22:42:01 +0100

    updated model user

--- app/models/user.js
+++ app/models/user.js
@@ -7,12 +7,6 @@ var userSchema=mongoose.Schema({
 		token:String,
 		email:String,
 		name:String
-	},
-	twitter:{
-		id:String,
-		token:String,
-		displayName:String,
-		username:String
 	}
 });
 
